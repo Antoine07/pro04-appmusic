c47a64ad8e9729df87725ad3e24f54611710221e
Antoine07 <antoine.lucsko@gmail.com>
2019-05-14 17:01:24 +0200

    login password

--- src/app/audio-player/audio-player.component.ts
+++ src/app/audio-player/audio-player.component.ts
@@ -32,7 +32,6 @@ export class AudioPlayerComponent implements OnInit {
           this.current++;
           this.ratio = Math.floor(this.current * (100 / this.total));
         } else {
-          clearInterval(interval);
           this.total = null;
           this.ratio = 0;
           this.current = 1;
@@ -42,7 +41,8 @@ export class AudioPlayerComponent implements OnInit {
             },
             error => console.log(error),
             () => console.log('completed...')
-          )
+          );
+          clearInterval(interval);
         }
       }, 1000)
     });
---
+++ src/app/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import firebase from '../environments/firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  // méthode d'authentification
+  auth(email: string, password: string): Promise<any> {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+}
