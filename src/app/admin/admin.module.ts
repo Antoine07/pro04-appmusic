68ae79ebd0794206153951a6960bddff072404cd
Antoine07 <antoine.lucsko@gmail.com>
2019-05-15 14:28:04 +0200

    admin start dashboard

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-// directives standards d'Angular comme ngFor, ngIf 
+// directives standards d'Angular comme ngFor, ngIf, ...
 import { CommonModule } from '@angular/common';
 import { AlbumComponent } from './album/album.component';
 
