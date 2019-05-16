868e1688811d1cfc03ba38e723c58a84c66456f6
Antoine07 <antoine.lucsko@gmail.com>
2019-05-16 17:03:22 +0200

    add post

--- src/app/admin/add-album/add-album.component.ts
+++ src/app/admin/add-album/add-album.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { AlbumService } from 'src/app/album.service';
 import { Router } from '@angular/router';
+import { Album } from 'src/app/albums';
 
 @Component({
   selector: 'app-add-album',
@@ -12,19 +13,21 @@ export class AddAlbumComponent implements OnInit {
 
   albumForm: FormGroup;
 
-  album: any;
+  album: Album;
 
   constructor(
     private fb: FormBuilder,
     private aS: AlbumService,
     private router: Router
   ) {
+    // STUB
     this.album = {
-      name: '',
-      title: '',
-      ref: '', // optional
-      duration: 0,
-      description: '',
+      id: Math.random().toString(32),
+      name: 'Albert',
+      title: Math.random().toString(32),
+      ref: '45FTR', // optional
+      duration: Math.floor(Math.random() * 960),
+      description: 'une description ...',
       status: 'off'
     }
   }
@@ -39,28 +42,46 @@ export class AddAlbumComponent implements OnInit {
     // utilisez les validators suivants pattern, max, required, minLength(5)
     this.albumForm = this.fb.group(
       {
+        id: this.album.id,
         name: new FormControl(this.album.name, [
           Validators.required,
           Validators.minLength(5)
         ]),
-        status : this.album.status
+        title: new FormControl(this.album.title, [
+          Validators.required,
+        ]),
+        ref: new FormControl(this.album.ref, [
+          Validators.required,
+          Validators.pattern("[a-zA-Z0-9\_]{5}") // regex pour vérifire une syntaxe
+        ]),
+        duration: new FormControl(this.album.duration, [
+          Validators.required,
+          Validators.max(960)
+        ]),
+        description: this.album.description,
+        status: this.album.status
       }
     );
   }
 
   // getter pour récupérer les messages d'erreur dans le formulaire cote html
   get name() { return this.albumForm.get('name'); }
+  get title() { return this.albumForm.get('title'); }
+  get ref() { return this.albumForm.get('ref'); }
+  get duration() { return this.albumForm.get('duration'); }
+  get description() { return this.albumForm.get('description'); }
 
   onSubmit() {
 
-    console.log(
-      this.albumForm.value['status'], 
-      this.albumForm.value['name']
-      );
-    
-      // au final on utilisera this.albumForm pour créer un nouvel album en base de données
-      // ici on doit ajouter un nouvel album ... AlbumService addAlbum méthode pour insérer un nouvel album
-  }
+    const album = this.albumForm.value;
 
+    this.aS.addAlbum(album).subscribe((snap : {name : string}) => {
+      album.id = snap.name;
+      this.aS.updateAlbum(snap.name, album).subscribe(() => {
+        console.log('updated ...')
+      })
+    });
+
+  }
 
 }
