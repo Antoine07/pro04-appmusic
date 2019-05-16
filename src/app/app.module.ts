d68bc7c0f18bbca03c21c8e47aea63c55f198056
Antoine07 <antoine.lucsko@gmail.com>
2019-05-16 11:09:05 +0200

    installation configuration sharemodule forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module'; // plugin ou extension
-import { FormsModule } from '@angular/forms';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminModule } from './admin/admin.module';
@@ -32,7 +32,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     ShareModule,
