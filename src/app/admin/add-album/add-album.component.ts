ecebbb0757d24735741a5756ae56a38236dcc1b5
Antoine07 <antoine.lucsko@gmail.com>
2019-05-17 11:55:52 +0200

    add album with dynamic field

--- src/app/admin/add-album/add-album.component.ts
+++ src/app/admin/add-album/add-album.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { AlbumService } from 'src/app/album.service';
 import { Router } from '@angular/router';
 import { Album } from 'src/app/albums';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-album',
@@ -12,7 +13,7 @@ import { Album } from 'src/app/albums';
 export class AddAlbumComponent implements OnInit {
 
   albumForm: FormGroup;
-
+  messageError: string;
   album: Album;
 
   constructor(
@@ -28,12 +29,33 @@ export class AddAlbumComponent implements OnInit {
       ref: '45FTR', // optional
       duration: Math.floor(Math.random() * 960),
       description: 'une description ...',
-      status: 'off'
+      status: 'off',
+      priceTTC: 0,
+      priceHT: 0,
+      proposition: 100,
+      price : 0
     }
   }
 
   ngOnInit() {
     this.initAlbum();
+
+    const proposition = 100;
+    // Observable 
+    // insérer une donnée dans un champ et modifier dynamiquement les autres champs
+    this.albumForm.get('proposition').valueChanges.pipe(
+      map(proposition => { return { 
+        pTTC: Math.floor(proposition * 1.05), pHT: proposition, pInit: proposition } })
+    ).subscribe(
+      p => {
+        const priceTTC = p.pTTC < proposition ? "Error..." : p.pTTC;
+
+        // hydratation des données de ton formulaire de manière dynamique
+        this.albumForm.controls['price'].setValue(p.pInit);
+        this.albumForm.controls['priceHT'].setValue(p.pHT);
+        this.albumForm.controls['priceTTC'].setValue(priceTTC);
+      }
+    );
   }
 
   initAlbum() {
@@ -59,7 +81,14 @@ export class AddAlbumComponent implements OnInit {
           Validators.max(960)
         ]),
         description: this.album.description,
-        status: this.album.status
+        status: this.album.status,
+        price: this.album.price,
+        priceTTC: this.album.priceTTC,
+        priceHT: this.album.priceHT,
+        proposition: new FormControl(this.album.proposition, [
+          Validators.required,
+          Validators.min(100),
+        ]),
       }
     );
   }
@@ -70,16 +99,22 @@ export class AddAlbumComponent implements OnInit {
   get ref() { return this.albumForm.get('ref'); }
   get duration() { return this.albumForm.get('duration'); }
   get description() { return this.albumForm.get('description'); }
+  get price() { return this.albumForm.get('price'); }
+  get proposition() { return this.albumForm.get('proposition'); }
+  get priceTTC() { return this.albumForm.get('priceTTC'); }
 
   onSubmit() {
 
     const album = this.albumForm.value;
+    this.albumForm.value['proposition'] = parseInt(this.albumForm.value['proposition']);
 
-    this.aS.addAlbum(album).subscribe((snap : {name : string}) => {
+    this.aS.addAlbum(album).subscribe((snap: { name: string }) => {
       album.id = snap.name;
       this.aS.updateAlbum(snap.name, album).subscribe(() => {
-        console.log('updated ...')
-      })
+        this.router.navigate(['/dashboard'], { queryParams: { message: 'success add' } })
+      },
+        error => this.messageError = "Error store album database ..."
+      )
     });
 
   }
--- src/app/admin/album/album.component.ts
+++ src/app/admin/album/album.component.ts
@@ -3,6 +3,7 @@ import { AlbumService } from 'src/app/album.service';
 import { Album } from 'src/app/albums';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-album',
@@ -13,11 +14,13 @@ export class AlbumComponent implements OnInit {
 
   albums: Observable<Album[]>;
   changePerpage: number;
+  message: string;
 
-  constructor(private aS: AlbumService) {
+  constructor(private aS: AlbumService, private route: ActivatedRoute ) {
     // nombre d'albums par page dans l'administration
     // en fonction des variables d'environement
     this.changePerpage = environment.perPageAdmin;
+    this.message = this.route.snapshot.paramMap.get('message');
   }
 
   ngOnInit() {
